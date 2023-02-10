be4c116f060c6aafb8042089f4782eb9c6774889
Claude James Bond :) <bernard.claude66@icloud.com>
2023-02-10 15:46:34 -0500

    added new states

--- geolocation.py
+++ geolocation.py
@@ -6,11 +6,12 @@ from geolocation import states, postal codes,
 import pandas as pd
 def processdata():
     # Loading and reading the CSV file.
-    df = pd.read_csv('collision_accident.csv', delimiter = ',')
+    df = pd.read_csv('collision_accident.csv', delimiter = '0')
     # Converting the CSV data to a list.
     df = df.to_dict(orient = 'records')
     return df
   
   state= {florida} return key = 1 'state'
-  
-  
+  state= {new-jersey} return key = 1 'state'
+  state= {new-york} return key = 1 'state' 
+  state= {georgia} return key = 1 'state'
