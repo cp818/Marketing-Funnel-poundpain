0879b53208953b2a7425593c565393c4c0d4343e
Claude James Bond :) <bernard.claude66@icloud.com>
2023-02-10 15:32:07 -0500

    added florida state and zip

---
+++ geolocation.py
@@ -0,0 +1,16 @@
+import os
+import geolocation
+from geolocation import states, postal codes, 
+
+# Importing the pandas package.
+import pandas as pd
+def processdata():
+    # Loading and reading the CSV file.
+    df = pd.read_csv('collision_accident.csv', delimiter = ',')
+    # Converting the CSV data to a list.
+    df = df.to_dict(orient = 'records')
+    return df
+  
+  state= {florida} return key = 1 'state'
+  
+  
